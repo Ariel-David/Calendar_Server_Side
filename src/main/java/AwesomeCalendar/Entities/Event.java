6c3084f932ccc173f155e4be9dcad5d3e4a93c09
Figglophobia <gideon.jaffe@gmail.com>
2022-12-16 10:58:37 +0200

    created entities and basic flow

--- src/main/java/AwesomeCalendar/Entities/Event.java
+++ src/main/java/AwesomeCalendar/Entities/Event.java
@@ -1,4 +1,93 @@
 package AwesomeCalendar.Entities;
 
+import javax.persistence.*;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.OffsetTime;
+
+@Entity
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private OffsetTime time;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private Duration duration;
+
+    @Column
+    private String location;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+//    @Column
+//    private List<File> attachments;
+
+    public Event() {
+    }
+
+    public OffsetTime getTime() {
+        return time;
+    }
+
+    public void setTime(OffsetTime time) {
+        this.time = time;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Duration getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+//    public List<File> getAttachments() {
+//        return attachments;
+//    }
+//
+//    public void setAttachments(List<File> attachments) {
+//        this.attachments = attachments;
+//    }
 }
--- src/main/java/AwesomeCalendar/Entities/Role.java
+++ src/main/java/AwesomeCalendar/Entities/Role.java
@@ -1,4 +1,74 @@
 package AwesomeCalendar.Entities;
 
+import javax.persistence.*;
+
+@Entity
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne
+    private Event event;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne
+    private User user;
+
+    @Column(nullable = false)
+    private RoleType roleType;
+
+    @Column(nullable = false)
+    private StatusType statusType;
+
+    public Role() {
+    }
+
+    public Role(Event event, User user, RoleType roleType, StatusType statusType) {
+        this.event = event;
+        this.user = user;
+        this.roleType = roleType;
+        this.statusType = statusType;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public StatusType getStatusType() {
+        return statusType;
+    }
+
+    public void setStatusType(StatusType statusType) {
+        this.statusType = statusType;
+    }
+
+    public enum RoleType {
+        ORGANIZER, ADMIN, GUEST
+    }
+
+    public enum StatusType {
+        APPROVED, REJECTED, TENTATIVE
+    }
 }
