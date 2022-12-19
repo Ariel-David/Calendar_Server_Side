f3b6a02ad428fc83d1a38c155e2a2b35f4cb9c9f
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2022-12-19 13:11:10 +0200

    added createRole methods for new invited users

--- src/main/java/AwesomeCalendar/Repositories/RoleRepo.java
+++ src/main/java/AwesomeCalendar/Repositories/RoleRepo.java
@@ -1,7 +1,10 @@
 package AwesomeCalendar.Repositories;
 
+import AwesomeCalendar.Entities.Event;
 import AwesomeCalendar.Entities.Role;
+import AwesomeCalendar.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepo extends JpaRepository<Role, Long> {
+    Role findByEventAndUser(Event event, User user);
 }
