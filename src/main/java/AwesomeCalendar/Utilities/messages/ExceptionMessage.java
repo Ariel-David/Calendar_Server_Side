cf5f7c3710d1c889797218b24b299afbb1fe5410
AmerSamer <73889178+AmerSamer@users.noreply.github.com>
2022-12-21 16:19:32 +0200

    added ExceptionMessages to handle all the exception messages

---
+++ src/main/java/AwesomeCalendar/Utilities/messages/ExceptionMessage.java
@@ -0,0 +1,11 @@
+package AwesomeCalendar.Utilities.messages;
+
+public class ExceptionMessage {
+    public static String invalidEmailMessage = " Invalid Email, Please try again. Example: 'sample@gmail.com' ";
+    public static String invalidPasswordMessage = " Invalid password, Please try again.";
+    public static String requiredFieldMessage = "Required field";
+    public static String somethingWrongMessage = "Something wrong, please try again later";
+    public static String invalidStatusMessage = "Invalid Status, Please try again.";
+    public static String invalidEventIdMessage = "Invalid eventId";
+    public static String FieldsAdminCantUpdateMessage = "Admins can not update title, start date or end date";
+}
