78a72d7752091b181a1fc262d97df1610b4fd05f
Ariel <arieldavid642@gmail.com>
2022-12-21 10:09:56 +0200

    Add email notifications

---
+++ src/main/java/AwesomeCalendar/Services/emailService.java
@@ -0,0 +1,34 @@
+package AwesomeCalendar.Services;
+
+import AwesomeCalendar.Entities.User;
+import AwesomeCalendar.Utilities.Email;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class emailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmailNotification(String userEmail,String body) {
+        Email email = new Email.Builder().to(userEmail).subject("You Have A New Notification!").content(body).build();
+        mailSender.send(email.convertIntoMessage());
+    }
+    public void userStatusChanged(String userEmail){
+        sendEmailNotification(userEmail,"You have a new event invitation");
+    }
+    public void eventDataChanged(String userEmail){
+        sendEmailNotification(userEmail,"Event data changed");
+    }
+    public void eventCanceled(String userEmail){
+        sendEmailNotification(userEmail,"Event canceled");
+    }
+    public void userUninvitedFromEvent(String userEmail){
+        sendEmailNotification(userEmail,"You uninvited from event");
+    }
+    public void notificationUpcomingEvents(String userEmail){
+        sendEmailNotification(userEmail,"Your event is upcoming!");
+    }
+
+}
