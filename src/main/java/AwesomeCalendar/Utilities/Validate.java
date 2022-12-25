463bb2882886c6f9a0fc7e6e4267be86c3d75d7b
Figglophobia <gideon.jaffe@gmail.com>
2022-12-25 22:59:25 +0200

    utils::validate tests

--- src/main/java/AwesomeCalendar/Utilities/Validate.java
+++ src/main/java/AwesomeCalendar/Utilities/Validate.java
@@ -12,10 +12,15 @@ public class Validate {
     private static final Pattern VALID_PASSWORD_REGEX =
             Pattern.compile("^[0-9]{5,10}$", Pattern.CASE_INSENSITIVE);
 
+    /**
+     * check if the email is in the correct format.
+     * @param email the string to check.
+     * @return true if the string is in the correct format, and false if it is not.
+     */
     public static boolean email(String email) {
 
         logger.info("email validation has begun: ");
-        boolean status = VALID_EMAIL_ADDRESS_REGEX.matcher(email).find();
+        boolean status = email != null && VALID_EMAIL_ADDRESS_REGEX.matcher(email).find();
 
         if (status) {
             logger.info("email is valid");
@@ -25,9 +30,14 @@ public class Validate {
         return status;
     }
 
+    /**
+     * check if the password is in the correct format.
+     * @param password the string to check.
+     * @return true if the string is in the correct format, and false if it is not.
+     */
     public static boolean password(String password) {
         logger.info("password validation has begun: ");
-        boolean status = VALID_PASSWORD_REGEX.matcher(password).find();
+        boolean status = password != null && VALID_PASSWORD_REGEX.matcher(password).find();
 
         if (status) {
             logger.info("password is valid");
