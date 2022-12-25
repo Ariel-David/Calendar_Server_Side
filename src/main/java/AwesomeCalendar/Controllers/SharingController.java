22687ef9f958e4a7ec49903434cc7b762cfd5013
Figglophobia <gideon.jaffe@gmail.com>
2022-12-25 08:50:01 +0200

    func to get all shared calendars with me

--- src/main/java/AwesomeCalendar/Controllers/SharingController.java
+++ src/main/java/AwesomeCalendar/Controllers/SharingController.java
@@ -6,8 +6,11 @@ import AwesomeCalendar.Entities.User;
 import AwesomeCalendar.Services.SharingService;
 import AwesomeCalendar.Utilities.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.CustomEditorConfigurer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static AwesomeCalendar.Utilities.messages.ExceptionMessage.*;
 import static AwesomeCalendar.Utilities.messages.SuccessMessages.*;
@@ -19,6 +22,13 @@ public class SharingController {
     @Autowired
     private SharingService sharingService;
 
+    /**
+     * shared a calendar with another user. the other user will be able to see this users public events
+     * even if he was not invited to them
+     * @param user the user that wants to share a calendar with another user
+     * @param userEmail the email of the other user that the user wants to share their calendar with
+     * @return the user that was shared the calendar
+     */
     @PostMapping("/share")
     public ResponseEntity<CustomResponse<UserDTO>> shareCalendar(@RequestAttribute("user") User user, @RequestParam String userEmail) {
         if (user == null) return ResponseEntity.badRequest().build();
@@ -35,6 +45,21 @@ public class SharingController {
             cResponse = new CustomResponse<>(null, null, e.getMessage());
             return ResponseEntity.badRequest().body(cResponse);
         }
+    }
 
+    /**
+     * gets all calendars shared with the user. the user should call this function
+     * before requesting calendars, so he can see what calendars he can access to
+     * @param user the user that wants to see what calendars are shared with him.
+     *             if called from a rest call, the server expects a token and the filter will change it to user object.
+     * @return all the calendars that are shared with the user.
+     */
+    @GetMapping("/sharedWithMe")
+    public ResponseEntity<CustomResponse<List<UserDTO>>> sharedWithMeCalendars(@RequestAttribute("user") User user) {
+        if (user == null) return ResponseEntity.badRequest().build();
+        List<User> sharedWithMeCalendars = user.getSharedWithMeCalendars();
+        sharedWithMeCalendars.add(user);
+        CustomResponse<List<UserDTO>> cResponse = new CustomResponse<>(UserDTO.convertUserListToUserDTOList(sharedWithMeCalendars), null, getSharedCalendarsSuccessfullyMessage);
+        return ResponseEntity.ok().body(cResponse);
     }
 }
