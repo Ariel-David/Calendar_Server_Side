0ebc78f03030181ab4f74f4a0d997948dcf436cf
Ariel <arieldavid642@gmail.com>
2022-12-25 11:28:45 +0200

    add notification settings

--- src/main/java/AwesomeCalendar/Entities/GitHubUser.java
+++ src/main/java/AwesomeCalendar/Entities/GitHubUser.java
@@ -1,7 +1,4 @@
 package AwesomeCalendar.Entities;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
