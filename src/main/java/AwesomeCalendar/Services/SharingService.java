55924c9dc3e1f38476bd54a6df31c685800462f3
Figglophobia <gideon.jaffe@gmail.com>
2022-12-26 09:34:35 +0200

    logs/added logger to sharing service

--- src/main/java/AwesomeCalendar/Services/SharingService.java
+++ src/main/java/AwesomeCalendar/Services/SharingService.java
@@ -2,6 +2,8 @@ package AwesomeCalendar.Services;
 
 import AwesomeCalendar.Entities.User;
 import AwesomeCalendar.Repositories.UserRepo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,8 @@ import java.util.List;
 
 @Service
 public class SharingService {
+
+    private static final Logger logger = LogManager.getLogger(SharingService.class);
 
     @Autowired
     private UserRepo userRepository;
@@ -25,15 +29,19 @@ public class SharingService {
      */
     public User shareCalendar(User user, String sharedWithEmail) {
         if (user == null) {
+            logger.debug("error sharing calendar - User cant be null");
             throw new IllegalArgumentException("User cant be null");
         }
         User sharedWith = userRepository.findByEmail(sharedWithEmail);
         if (sharedWith == null) {
+            logger.debug("error sharing calendar - email does not correspond to a valid user");
             throw new IllegalArgumentException("Invalid user email");
         }
         if (sharedWith.getSharedWithMeCalendars().contains(user)) {
+            logger.debug("error sharing calendar - user:" + user.getId() + " already shared their calendar with user:" + sharedWith.getId());
             throw new IllegalArgumentException("Calendar already shared with this user!");
         }
+        logger.info("user:" + user.getId() + " shared their calendar with user:" + sharedWith.getId());
         sharedWith.addSharedCalendar(user);
         userRepository.save(sharedWith);
         return sharedWith;
@@ -49,20 +57,16 @@ public class SharingService {
      */
     public List<User> isShared(User user, List<String> usersEmail) {
         if (user == null) {
+            logger.debug("error checking shared emails - user cant be null");
             throw new IllegalArgumentException("user cant be null");
         }
         if (usersEmail == null) {
+            logger.debug("error checking shared emails - emails list cant be null");
             throw new IllegalArgumentException("usersEmail cant be null");
         }
-        List<User> usersCalendars = new ArrayList<>();
-        for (String email : usersEmail) {
-            User byEmail = userRepository.findByEmail(email);
-            if (byEmail == null) {
-                throw new IllegalArgumentException("user doesn't exist");
-            }
-            usersCalendars.add(byEmail);
-        }
+        List<User> usersCalendars = userRepository.findByEmailIn(usersEmail);
         boolean containsAll;
+        logger.info("starting checking if user has permission to calendars for email list");
         if (usersCalendars.contains(user)) {
             usersCalendars.remove(user);
             containsAll = user.getSharedWithMeCalendars().containsAll(usersCalendars);
@@ -71,8 +75,10 @@ public class SharingService {
             containsAll = user.getSharedWithMeCalendars().containsAll(usersCalendars);
         }
         if (!containsAll) {
+            logger.debug("error checking shared emails - user doesn't have access to all emails in list");
             throw new IllegalArgumentException("calendar not shared with user");
         }
+        logger.info("finished checking if user has permission to calendars for email list");
         return usersCalendars;
     }
 }
